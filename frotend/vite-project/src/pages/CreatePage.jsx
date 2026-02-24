dbfcd202976543ab42249349ba8c559667cfd618
SanoAngella <sanoangella9@gmail.com>
2026-02-24 20:11:13 +0200

    react study

--- frotend/vite-project/src/pages/CreatePage.jsx
+++ frotend/vite-project/src/pages/CreatePage.jsx
@@ -2,8 +2,31 @@ import React from "react";
 import { useNavigate} from "react-router-dom";
 
 const CreatePage = () => {
+<<<<<<< HEAD
   const [skillName, setSkillName] = useState('');
   const [level, setLevel]= useState('Beginner')
   const navigate = useNavigate();
 
 }
+=======
+  const navigate = useNavigate();
+  const handleCreate = () =>{
+    // simulate skill creation
+    setTimeout(() => {
+      navigate("/");
+
+    })
+  }
+  return (
+    <div>
+      <h1>Create new Skill</h1>
+      <button onClick={handleCreate}>Create Skill</button>
+
+
+    </div>
+
+  )
+}
+
+export default CreatePage
+>>>>>>> 82f2696 (react changes)
