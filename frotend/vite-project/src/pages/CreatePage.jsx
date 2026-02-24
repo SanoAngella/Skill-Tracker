e68370e9f9532399996fb2cd71a46c0a8d40ec60
SanoAngella <sanoangella9@gmail.com>
2026-02-24 21:47:41 +0200

    cleanup of CreatePage merge conflict

--- frotend/vite-project/src/pages/CreatePage.jsx
+++ frotend/vite-project/src/pages/CreatePage.jsx
@@ -1,32 +1,30 @@
-import React from "react";
-import { useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreatePage = () => {
-<<<<<<< HEAD
   const [skillName, setSkillName] = useState('');
-  const [level, setLevel]= useState('Beginner')
+  const [level, setLevel] = useState('Beginner');
   const navigate = useNavigate();
 
-}
-=======
-  const navigate = useNavigate();
-  const handleCreate = () =>{
-    // simulate skill creation
+  const handleCreate = () => {
+    
     setTimeout(() => {
       navigate("/");
+    }, 500);
+  };
 
-    })
-  }
   return (
     <div>
       <h1>Create new Skill</h1>
+      <input 
+        type="text" 
+        placeholder="Skill Name" 
+        value={skillName} 
+        onChange={(e) => setSkillName(e.target.value)} 
+      />
       <button onClick={handleCreate}>Create Skill</button>
-
-
     </div>
+  );
+};
 
-  )
-}
-
-export default CreatePage
->>>>>>> 82f2696 (react changes)
+export default CreatePage;
